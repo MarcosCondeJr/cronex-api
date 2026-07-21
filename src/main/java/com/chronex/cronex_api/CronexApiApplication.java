06fcc73290693f63a38daa83eca165454880461b
marcoscondejr <marcos14conde@hotmail.com>
2026-07-20 23:15:30 -0300

    feat: criando configuração para carregamento do env

--- src/main/java/com/chronex/cronex_api/CronexApiApplication.java
+++ src/main/java/com/chronex/cronex_api/CronexApiApplication.java
@@ -3,10 +3,13 @@ package com.chronex.cronex_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.chronex.cronex_api.config.DotenvLoader;
+
 @SpringBootApplication
 public class CronexApiApplication {
 
 	public static void main(String[] args) {
+		DotenvLoader.init();
 		SpringApplication.run(CronexApiApplication.class, args);
 	}
 
