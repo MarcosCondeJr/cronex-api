488b3d56a96d2cce39950415eb20c42ce23c1fd7
marcoscondejr <marcos14conde@hotmail.com>
2026-07-20 23:31:59 -0300

    feat: criando migration para criação de tabela de usuários

---
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -0,0 +1,13 @@
+CREATE TYPE role_user AS ENUM ('ADMIN', 'USER');
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(150) NOT NULL,
+    login VARCHAR(11) NOT NULL UNIQUE,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role role_user NOT NULL DEFAULT 'USER',
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP
+);
