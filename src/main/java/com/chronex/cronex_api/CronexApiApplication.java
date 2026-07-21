a6e9d4815df0ecdbc6c07cded463fe7d6e5f733e
marcoscondejr <marcos14conde@hotmail.com>
2026-07-20 23:06:46 -0300

    feat: first commit

---
+++ src/main/java/com/chronex/cronex_api/CronexApiApplication.java
@@ -0,0 +1,13 @@
+package com.chronex.cronex_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronex/cronex_api/CronexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronex.cronex_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
