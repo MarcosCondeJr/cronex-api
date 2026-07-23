8731c42f0c68f2017114395311920398f83d749c
marcoscondejr <marcos14conde@hotmail.com>
2026-07-23 00:36:11 -0300

    feat: criando entity de user e alterando estrutura da tabela de user

---
+++ src/main/java/com/chronex/cronex_api/entity/User.java
@@ -0,0 +1,53 @@
+package com.chronex.cronex_api.entity;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private boolean active;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
--- src/main/resources/db/migration/V1__create_table_users.sql
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -3,7 +3,6 @@ CREATE TYPE role_user AS ENUM ('ADMIN', 'USER');
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(150) NOT NULL,
-    login VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(150) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role role_user NOT NULL DEFAULT 'USER',
