2c6c46fc876443be731ec3e27aa4be5a7b6afead
marcoscondejr <marcos14conde@hotmail.com>
2026-07-26 00:33:54 -0300

    refactor: alterações na estrutura e métodos relacionados da autenticação

--- src/main/java/com/chronex/cronex_api/controller/AuthController.java
+++ src/main/java/com/chronex/cronex_api/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.chronex.cronex_api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +13,7 @@ import com.chronex.cronex_api.dto.auth.AuthResponse;
 import com.chronex.cronex_api.dto.user.UserRequest;
 import com.chronex.cronex_api.dto.user.UserResponse;
 import com.chronex.cronex_api.service.AuthService;
+import com.chronex.cronex_api.service.UserService;
 
 import jakarta.validation.Valid;
 
@@ -21,23 +25,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AuthController {
 
     private AuthService authService;
+    private UserService userService;
+    private AuthenticationManager authenticationManager;
 
-    public AuthController(AuthService authService)
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager)
     {
         this.authService = authService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid AuthRequest request)
     {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authentication = authenticationManager.authenticate(authToken);
+
         this.authService.login(request);
+
         return ResponseEntity.ok(null);
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@Valid UserRequest request)
     {
-        UserResponse response = this.authService.register(request);
+        UserResponse response = this.userService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/chronex/cronex_api/service/UserService.java
+++ src/main/java/com/chronex/cronex_api/service/UserService.java
@@ -1,15 +1,48 @@
 package com.chronex.cronex_api.service;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.chronex.cronex_api.dto.user.UserRequest;
+import com.chronex.cronex_api.dto.user.UserResponse;
+import com.chronex.cronex_api.entity.User;
+import com.chronex.cronex_api.enums.UserRole;
+import com.chronex.cronex_api.exception.ConflictException;
 import com.chronex.cronex_api.repository.UserRepository;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    /**
+     * Registra um novo usuário no sistema.
+     * 
+     * @param request
+     * @return
+     */
+    public UserResponse register(UserRequest request) {
+        Optional<User> existingUser = userRepository.findByEmail(request.email());
+
+        if (!existingUser.isEmpty()) {
+            throw new ConflictException("Email já cadastrado");
+        }
+
+        User user = new User();
+        user.setName(request.name());
+        user.setEmail(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setRole(UserRole.USER);
+
+        userRepository.save(user);
+
+        return UserResponse.fromEntity(user);
     }
 }
