884afb2a9c574d996a01029a38baa86e09e0105b
Marcos Conde <marcos14conde@hotmail.com>
2026-07-31 08:35:09 -0300

    feat: adicionando configuração para o retorno dos registros, para vim no formato Via Dto do JPA

---
+++ src/main/java/com/chronex/cronex_api/config/SpringDataConfig.java
@@ -0,0 +1,12 @@
+package com.chronex.cronex_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
+public class SpringDataConfig {
+}
+
